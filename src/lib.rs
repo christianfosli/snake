510f1343ee7d603171d4d4b8d951182ee1c14f75
Christian Fosli <cfosli@gmail.com>
2020-03-15 19:59:32 +0100

    Add a Snake struct, start on Impl and initial drawing

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,65 @@ extern crate wasm_bindgen;
 extern crate web_sys;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::HtmlElement;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, HtmlElement};
+
+pub struct Snake {
+    body: Vec<Position>,
+    direction: Direction,
+    thickness: f64,
+}
+
+impl Snake {
+    pub fn new() -> Snake {
+        Snake {
+            body: vec![Position { x: 0.0, y: 0.0 }],
+            direction: Direction::Right,
+            thickness: 25.0,
+        }
+    }
+    fn next_position(&self) -> Position {
+        let head = self.body.first().unwrap();
+        match self.direction {
+            Direction::Right => Position {
+                x: head.x + self.thickness,
+                ..*head
+            },
+            Direction::Down => Position {
+                y: head.y + self.thickness,
+                ..*head
+            },
+            Direction::Left => Position {
+                x: head.x - self.thickness,
+                ..*head
+            },
+            Direction::Up => Position {
+                y: head.y - self.thickness,
+                ..*head
+            },
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Direction {
+    Right,
+    Down,
+    Left,
+    Up,
+}
+
+#[derive(Copy, Clone)]
+struct Position {
+    x: f64,
+    y: f64,
+}
 
 // Called by our JS entry point
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     add_canvas()?;
+    let snake = Snake::new();
+    draw_snake(&snake)?;
 
     Ok(())
 }
@@ -26,12 +79,45 @@ fn add_canvas() -> Result<(), JsValue> {
     Ok(())
 }
 
+fn get_canvas_context() -> Result<CanvasRenderingContext2d, JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .expect("no canvas element could be found")
+        .dyn_into::<HtmlCanvasElement>()?;
+
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()?;
+
+    Ok(context)
+}
+
+fn draw_snake(snake: &Snake) -> Result<(), JsValue> {
+    let context = get_canvas_context()?;
+    for pos in snake.body.iter() {
+        context.fill_rect(pos.x, pos.y, snake.thickness, snake.thickness);
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn pass() {
-        assert_eq!(1, 1);
+    fn it_initially_moves_right() {
+        let snake = Snake::new();
+        let initial_dir = &snake.direction;
+        assert!(matches!(Direction::Right, initial_dir));
+    }
+
+    #[test]
+    fn next_position_is_thickness_away_from_head() {
+        let snake = Snake::new();
+        let head = snake.body.first().expect("snake should have a body");
+        let next = snake.next_position();
+        assert_eq!(next.x, head.x + snake.thickness);
     }
 }
