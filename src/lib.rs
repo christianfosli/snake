084d2f9bbd36a7f0c449727ce0ddea2a52a83448
Christian Fosli <cfosli@gmail.com>
2020-03-16 20:52:15 +0100

    Move structs and impl into their own file/mod (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -4,68 +4,8 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, HtmlElement};
 
-pub struct Snake {
-    body: Vec<Position>,
-    direction: Direction,
-    thickness: f64,
-}
-
-impl Snake {
-    pub fn new() -> Snake {
-        Snake {
-            body: vec![Position { x: 0.0, y: 0.0 }],
-            direction: Direction::Right,
-            thickness: 25.0,
-        }
-    }
-
-    fn next_position(&self) -> Position {
-        let head = self.head();
-        match self.direction {
-            Direction::Up => Position {
-                y: head.y - self.thickness,
-                ..*head
-            },
-            Direction::Right => Position {
-                x: head.x + self.thickness,
-                ..*head
-            },
-            Direction::Down => Position {
-                y: head.y + self.thickness,
-                ..*head
-            },
-            Direction::Left => Position {
-                x: head.x - self.thickness,
-                ..*head
-            },
-        }
-    }
-
-    fn head(&self) -> &Position {
-        self.body
-            .last()
-            .expect("snake has head because it has no body")
-    }
-
-    fn move_along(&mut self) {
-        self.body.remove(0);
-        self.body.push(self.next_position());
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-#[derive(Copy, Clone)]
-struct Position {
-    x: f64,
-    y: f64,
-}
+mod snake;
+use crate::snake::*;
 
 // Called by our JS entry point
 #[wasm_bindgen]
@@ -115,23 +55,4 @@ fn draw_snake(snake: &Snake) -> Result<(), JsValue> {
         context.fill_rect(pos.x, pos.y, snake.thickness, snake.thickness);
     }
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_initially_moves_right() {
-        let snake = Snake::new();
-        assert_eq!(Direction::Right, snake.direction);
-    }
-
-    #[test]
-    fn next_position_is_thickness_away_from_head() {
-        let snake = Snake::new();
-        let head = snake.body.first().expect("snake should have a body");
-        let next = snake.next_position();
-        assert_eq!(next.x, head.x + snake.thickness);
-    }
 }
