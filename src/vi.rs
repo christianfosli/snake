e9131180dedefd0ad426a5cb7834006b68c881b0
Christian Fosli <cfosli@gmail.com>
2020-03-29 17:46:28 +0200

    Add `Vi` struct + event listener + update to 2018 edition (#6)
    * Adds Vi struct for translating key events to directions
    
    * Adds an event listener for keydown
    
    BUT I can't figure out how to change the direction in one block without invalidating it
    for being used in the interval closure. So the snake doesn't actually change directions yet...
    
    Adding "Edition=2018" to cargo.toml gave a lot of advantages,
    such as using `.await` and not having to put `extern crate` everywhere

---
+++ src/vi.rs
@@ -0,0 +1,44 @@
+use crate::Direction;
+use futures::channel::mpsc;
+use futures::stream::Stream;
+use gloo_events::EventListener;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use wasm_bindgen::JsCast;
+use web_sys::{EventTarget, KeyboardEvent};
+
+pub struct Vi {
+    pub receiver: mpsc::UnboundedReceiver<Direction>,
+    listener: EventListener,
+}
+
+impl Vi {
+    pub fn new(target: &EventTarget) -> Self {
+        let (sender, receiver) = mpsc::unbounded();
+        let listener = EventListener::new(&target, "keydown", move |event| {
+            let event: &KeyboardEvent = event.dyn_ref::<KeyboardEvent>().unwrap();
+            let key: &str = &event.key();
+            let dir: Option<Direction> = match key {
+                "h" => Some(Direction::Left),
+                "j" => Some(Direction::Down),
+                "k" => Some(Direction::Up),
+                "l" => Some(Direction::Right),
+                _ => None,
+            };
+            match dir {
+                Some(dir) => sender.unbounded_send(dir).unwrap(),
+                None => (),
+            };
+        });
+
+        Self { receiver, listener }
+    }
+}
+
+impl Stream for Vi {
+    type Item = Direction;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        Pin::new(&mut self.receiver).poll_next(cx)
+    }
+}
