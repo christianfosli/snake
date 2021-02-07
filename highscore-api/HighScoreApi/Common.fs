cc43cbe2a417fbce174c9cb747221b120a7abf16
Christian Fosli <christian.fosli@capgemini.com>
2021-02-07 15:16:47 +0100

    feat(highscore-api): add retry policy when querying topten

--- highscore-api/HighScoreApi/Common.fs
+++ highscore-api/HighScoreApi/Common.fs
@@ -14,9 +14,10 @@ module Types =
         let value (Score score) = score
 
         let create n =
-            if n < 0 || n > max
-            then Error(ValidationError "score out of range")
-            else Ok(Score n)
+            if n < 0 || n > max then
+                Error(ValidationError "score out of range")
+            else
+                Ok(Score n)
 
     // --- Domain Models --- //
     type HighScore =
@@ -27,6 +28,7 @@ module Types =
 module DbUtils =
     open Types
     open Dapper
+    open Polly
     open Microsoft.Data.SqlClient
 
     let connString =
@@ -39,10 +41,33 @@ module DbUtils =
 
         override __.Parse value =
             let value = value :?> int
+
             match value |> Score.create with
             | Ok score -> score
-            | Error e -> sprintf "Error parsing to Score: %A" e |> failwith
+            | Error e -> failwithf "Error parsing to Score: %A" e
 
     let dbConnection connString =
         SqlMapper.AddTypeHandler(SqlScoreHandler())
         new SqlConnection(connString)
+
+    let queryWithRetries<'entity> (connection: SqlConnection) (sql: string) =
+        let retryPolicy =
+            Policy
+                .Handle<SqlException>()
+                .WaitAndRetryAsync(
+                    seq {
+                        TimeSpan.FromSeconds 2.0
+                        TimeSpan.FromSeconds 4.0
+                        TimeSpan.FromSeconds 6.0
+                    }
+                )
+
+        async {
+            try
+                let! res =
+                    retryPolicy.ExecuteAsync(fun () -> connection.QueryAsync<'entity>(sql))
+                    |> Async.AwaitTask
+
+                return Ok res
+            with ex -> return Error ex
+        }
--- highscore-api/HighScoreApi/TopTen.fs
+++ highscore-api/HighScoreApi/TopTen.fs
@@ -1,6 +1,5 @@
 namespace HighScoreApi
 
-open Dapper
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
@@ -9,19 +8,20 @@ open Microsoft.Data.SqlClient
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Primitives
 
-open Common.Types
 open Common.DbUtils
 open Common.Dto.HighScoreDto
+open Common.Types
 
 module TopTen =
 
     let topScores (connection: SqlConnection) =
         async {
             let! result =
-                connection.QueryAsync<HighScore> "select top(10) [UserName],[Score],[TimeStamp]
-                 from [highscores]
-                 order by [Score] desc, [TimeStamp] asc"
-                |> Async.AwaitTask
+                queryWithRetries<HighScore>
+                    connection
+                    "select top(10) [UserName],[Score],[TimeStamp]
+                     from [highscores]
+                     order by [Score] desc, [TimeStamp] asc"
 
             do! connection.CloseAsync() |> Async.AwaitTask
 
@@ -31,14 +31,25 @@ module TopTen =
     [<FunctionName("TopTen")>]
     let run ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)>] req: HttpRequest) (log: ILogger) =
         async {
-            log.LogInformation "TopTen triggered"
-
             // It is not yet possible to configure CORS for az functions locally in containers
             // so we just fix headers (ref https://github.com/Azure/azure-functions-host/issues/5090)
             req.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", StringValues "*")
 
             let! topScores = connString |> dbConnection |> topScores
 
-            return OkObjectResult(topScores |> Seq.map fromHighScore) :> IActionResult
+            return
+                match topScores with
+                | Ok x ->
+                    Seq.length x
+                    |> sprintf "%d scores retrieved successfully"
+                    |> log.LogInformation
+
+                    Seq.map fromHighScore x |> OkObjectResult :> IActionResult
+                | Error e ->
+                    sprintf "Failed to get top ten: %A" e
+                    |> log.LogError
+
+                    failwith "An error occured"
+
         }
         |> Async.StartAsTask
