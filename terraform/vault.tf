23d2203d7dae1212d686eba2ed6a433404802e0a
Christian Fosli <christian.fosli@capgemini.com>
2021-07-27 17:03:16 +0200

    fix cycle

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -9,23 +9,25 @@ resource "azurerm_key_vault" "vault" {
 
   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions        = ["Get"]
-    secret_permissions     = ["Get", "Set", "Delete", "Purge", "Recover", "Restore"]
-    cerificate_permissions = ["Get"]
-  }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = azurerm_function_app.api.identity.0.object_id
-
-    key_permissions        = ["Get"]
-    secret_permissions     = ["Get"]
-    cerificate_permissions = ["Get"]
-  }
-
   tags = local.common_tags
+}
+
+resource "azurerm_key_vault_access_policy" "tfAgent" {
+  key_vault_id = azurerm_key_vault.vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions        = ["Get"]
+  secret_permissions     = ["Get", "Set", "Delete", "Purge", "Recover", "Restore"]
+  cerificate_permissions = ["Get"]
+}
+
+resource "azurerm_key_vault_access_policy" "funcApi" {
+  key_vault_id = azurerm_key_vault.vault.id
+  tenant_id    = azurerm_function_app.api.identity.0.tenant_id
+  object_id    = azurerm_function_app.api.identity.0.object_id
+
+  key_permissions        = ["Get"]
+  secret_permissions     = ["Get"]
+  cerificate_permissions = ["Get"]
 }
