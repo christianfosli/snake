806074e95d4971fd59a81364a52a7d9961eba6ae
Christian Fosli <christian.fosli@capgemini.com>
2021-07-27 21:40:32 +0200

    uh

--- terraform/app.tf
+++ terraform/app.tf
@@ -2,9 +2,13 @@ resource "azurerm_static_site" "app" {
   name                = "stapp-snake-${var.ENVIRONMENT}"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = "West Europe" # Norway not yet supported for azure static webapp
+  default_host_name   = "snake-${var.ENVIRONMENT}.azurestaticapps.net"
+  sku_tier            = "Standard" # Free tier does not support bring-your-own functions
+  sku_size            = "Standard" # Free tier does not support bring-your-own functions
   tags                = local.common_tags
 }
 
-# TODO / Manual Step
-# Linking function app and static web app together is not yet possible as IaC
-# Done manually in the portal following https://docs.microsoft.com/en-us/azure/static-web-apps/functions-bring-your-own
+# Manual Steps
+# - Associate with GitHub target repository
+# - Link function app and static web app together
+#   (https://docs.microsoft.com/en-us/azure/static-web-apps/functions-bring-your-own)
