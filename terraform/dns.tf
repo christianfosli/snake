ee925ed7c462ad59676bf663fe3fd763b02bf3cf
Christian Fosli <christian.fosli@capgemini.com>
2021-07-29 16:56:10 +0200

    then we got the rest

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -8,20 +8,20 @@ resource "azurerm_dns_zone" "playsnakePublic" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-#resource "azurerm_dns_cname_record" "app" {
-#  count = var.ENVIRONMENT == "prod" ? 1 : 0
-#
-#  name                = "www"
-#  zone_name           = azurerm_dns_zone.playsnakePublic.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  ttl                 = 300
-#  target_resource_id  = azurerm_static_site.app[0].id
-#}
-#
-#resource "azurerm_dns_cname_record" "highscoreApi" {
-#  name                = var.ENVIRONMENT == "prod" ? "highscores" : "highscores-${var.ENVIRONMENT}"
-#  zone_name           = "playsnake.no"  # hardcoded because sometimes different env
-#  resource_group_name = "rg-snake-prod" # hardcoded because sometimes different env
-#  ttl                 = 300
-#  target_resource_id  = azurerm_function_app.highScoreApi.id
-#}
+resource "azurerm_dns_cname_record" "app" {
+  count = var.ENVIRONMENT == "prod" ? 1 : 0
+
+  name                = "www"
+  zone_name           = azurerm_dns_zone.playsnakePublic[0].name
+  resource_group_name = azurerm_dns_zone.playsnakePublic[0].resource_group_name
+  ttl                 = 300
+  target_resource_id  = azurerm_static_site.app[0].id
+}
+
+resource "azurerm_dns_cname_record" "highscoreApi" {
+  name                = var.ENVIRONMENT == "prod" ? "highscores" : "highscores-${var.ENVIRONMENT}"
+  zone_name           = "playsnake.no"  # hardcoded because sometimes different env
+  resource_group_name = "rg-snake-prod" # hardcoded because sometimes different env
+  ttl                 = 300
+  target_resource_id  = azurerm_function_app.highScoreApi.id
+}
