8c247776c69ca0cd381a4ca516dea30f51d97fe1
Christian Fosli <christian.fosli@capgemini.com>
2024-01-27 21:14:12 +0100

    chore(terraform/keyvault): enable azure rbac authorization

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -4,19 +4,10 @@ resource "azurerm_key_vault" "vault" {
   resource_group_name        = data.azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
+  enable_rbac_authorization  = true
   purge_protection_enabled   = false
 
   sku_name = "standard"
 
   tags = local.common_tags
-}
-
-resource "azurerm_key_vault_access_policy" "tfAgent" {
-  key_vault_id = azurerm_key_vault.vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  key_permissions         = ["Get"]
-  secret_permissions      = ["Get", "Set", "Delete", "Purge", "Recover", "Restore"]
-  certificate_permissions = ["Get"]
 }
