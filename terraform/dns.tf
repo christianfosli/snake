9f592f49e3d324c163c11b47d15aa5bc123b20aa
Christian Fosli <christian.fosli@capgemini.com>
2024-01-27 12:27:13 +0100

    Revert "[tmp]: comment out dns"
    This reverts commit e570fee71a64a93c8fddd546c9be246cd56caecb.

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -20,24 +20,24 @@ resource "azurerm_dns_cname_record" "app" {
   tags                = local.common_tags
 }
 
-# resource "azurerm_dns_cname_record" "highScoreApi" {
-#   name                = var.ENVIRONMENT == "prod" ? "highscores" : "highscores-${var.ENVIRONMENT}"
-#   zone_name           = "playsnake.no"  # hardcoded because sometimes different env
-#   resource_group_name = "rg-snake-prod" # hardcoded because sometimes different env
-#   ttl                 = 300
-#   record              = azurerm_container_app.highscoreApi.ingress[0].fqdn
-#   tags                = local.common_tags
-# }
+resource "azurerm_dns_cname_record" "highScoreApi" {
+  name                = var.ENVIRONMENT == "prod" ? "highscores" : "highscores-${var.ENVIRONMENT}"
+  zone_name           = "playsnake.no"  # hardcoded because sometimes different env
+  resource_group_name = "rg-snake-prod" # hardcoded because sometimes different env
+  ttl                 = 300
+  record              = azurerm_container_app.highscoreApi.ingress[0].fqdn
+  tags                = local.common_tags
+}
 
-# resource "azurerm_dns_txt_record" "highScoreApiVerification" {
-#   name                = "asuid.${azurerm_dns_cname_record.highScoreApi.name}"
-#   zone_name           = "playsnake.no"  # hardcoded because sometimes different env
-#   resource_group_name = "rg-snake-prod" # hardcoded because cometimes different env
-#   ttl                 = 300
+resource "azurerm_dns_txt_record" "highScoreApiVerification" {
+  name                = "asuid.${azurerm_dns_cname_record.highScoreApi.name}"
+  zone_name           = "playsnake.no"  # hardcoded because sometimes different env
+  resource_group_name = "rg-snake-prod" # hardcoded because cometimes different env
+  ttl                 = 300
 
-#   record {
-#     value = azurerm_container_app.highscoreApi.custom_domain_verification_id
-#   }
+  record {
+    value = azurerm_container_app.highscoreApi.custom_domain_verification_id
+  }
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
